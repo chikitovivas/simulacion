8f4649dd3c4cbf6e8f1dcbccfb7e96f213510835
Administrador <Administrador@Equipo>
2015-12-08 13:41:41 -0430

    Json

---
+++ Consultorio_medico/src/Registro_cita/Cod_registrocita.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Registro_cita;
+//import JSON.JSON;
+/**
+ *
+ * @author Administrador
+ */
+public class Cod_registrocita {
+    
+}
