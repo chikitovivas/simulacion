4de02d62af87a66734782b69f31516467b7c701c
nico722 <nicolas.cardenas722@gmail.com>
2015-12-09 23:09:48 -0430

    Json citas listo

--- Consultorio_medico/src/JSON/PruebaJsonNico.java
+++ Consultorio_medico/src/JSON/PruebaJsonNico.java
@@ -5,8 +5,9 @@
  */
 package JSON;
 
-import Calendario_Consultorio.Ven_calendario;
-import java.io.IOException;
+import java.util.Date;
+import java.util.Iterator;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.http.HttpEntity;
@@ -16,6 +17,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
 
 /**
  *
@@ -28,16 +30,18 @@ public class PruebaJsonNico {
     
     }
     
-    public void Peticion_nico(int ci){
+    public void Peticion_Citas(int ci){
     
         // para prueba 
         ci=25620021;
         
         HttpClient client = new DefaultHttpClient();
         int id=1;
-        String dirLAN="169.254.105.188";// con router nico NO WIFI
-          //HttpGet get = new HttpGet("http://miconsultoriocal.no-ip.org:8000/cita/"+ci);
-          //HttpGet get = new HttpGet("http://localhost:8000/cita");   //para pedir toda la tabla cita
+        String dirLAN=
+                //"169.254.105.188";// con router nico NO WIFI
+                "miconsultoriocal.no-ip.org"; //Router remoto rafa
+ 
+        
           HttpGet get = new HttpGet("http://"+dirLAN+":8000/cita/"+ci);
 
         
@@ -45,43 +49,98 @@ public class PruebaJsonNico {
             HttpResponse response = client.execute(get);
             HttpEntity entity= response.getEntity();
             String content = EntityUtils.toString(entity);
-
-            System.out.println(response.toString());
-                System.out.println();System.out.println();
-             System.out.println(content);
-             JSONArray json = new JSONArray(content);
-                System.out.println();System.out.println();
-             System.out.println(json);
-          /*   
-            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            String line = "";
-            while ((line = rd.readLine()) != null) {
-             System.out.println(line);
-            }
-            */
-                      
             
-             
-             //PROBANDO PARA TRAER CADA TUPLA
-             System.out.println();System.out.println();
-             System.out.println(json.getJSONObject(0));
-             System.out.println(json.getJSONObject(0).get("nombrePaciente")); //para traer atributos
-             System.out.println(json.length());
-             
-             
+             JSONArray json = new JSONArray(content);      //JSON                       
+             crear_matriz(json);
              
         } catch (Exception ex) {
             Logger.getLogger(JSON.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-      
-      
+                  
     }
+    
+    private Date crear_date(Object fechahora){
+        Date aux=null;
+        String fech=fechahora.toString();
+
+        aux=new Date(Integer.parseInt(fech.substring(0, 4))-1900 //año
+                    ,Integer.parseInt(fech.substring(5, 7))-1    //mes
+                    ,Integer.parseInt(fech.substring(8, 10))     //dia
+                    ,Integer.parseInt(fech.substring(11, 13))   //hora
+                    ,Integer.parseInt(fech.substring(14, 16))   //minutos  
+                    ,Integer.parseInt(fech.substring(17, 19))); //segundos
+       
+        
+    return aux;
+    }
+    
+    
+    private JSONArray ordenar_json(JSONArray aux) throws JSONException{
+        JSONArray keys=aux;
+                    //System.out.println("primer keys");
+                    //System.out.println(keys);
+        Object jaux=keys.get(0);// guardo fecha nac
+        keys.put(0, keys.get(4));// muevo fecha hora       
+        keys.put(4, keys.get(6));// //muevo numero
+        keys.put(6, jaux);// muevo fecha nac del aux        
+        jaux=keys.get(1); //guardo nombre paciente                
+        keys.put(1, keys.get(2)); //muevo cedula
+        keys.put(2, jaux);//muevo nombre paciente del aux
+        jaux=keys.get(5);//guardo apellido
+        keys.put(5, keys.get(3)); //muevo correo
+        keys.put(3, jaux);// muevo apellido paciente del aux
+        
+                    //System.out.println("segundo keys");
+                    //System.out.println(keys);
+        
+        return keys;
+    }
+    
+    public Object[][] crear_matriz(JSONArray json) throws JSONException{
+        if (json==null)
+            return null;       
+        Object[][] aux=null;
+
+        /* EJEMPLO DEL JSON DEL SERVIDOR
+        [
+        {"fecha_nac":"1995-12-12","nombrePaciente":"Pato","ciPaciente":8990721,"fechahora":"2015-12-08 21:23:54","apellidoPaciente":"Mendoza","numero":"0321221"},
+        {"fecha_nac":null,"nombrePaciente":"Yolanda","ciPaciente":121442,"fechahora":"2015-12-19 12:00:00","apellidoPaciente":"Fernandez","numero":"0286961212"},
+        {"fecha_nac":"1989-12-06","nombrePaciente":"Marilu","ciPaciente":12144908,"fechahora":"2015-12-24 12:00:00","apellidoPaciente":"Machado","numero":"04140944678"}
+        ]        
+        */
+        int fil=json.length()
+           ,col=json.getJSONObject(0).length();
+        aux=new Object[fil][col];
+        JSONArray keys=json.getJSONObject(0).names();        
+        keys=ordenar_json(keys);
+        
+        
+        for(int i=0;i<fil;i++){
+            for(int j=0;j<col;j++){
+                switch(j){
+                    case 0:aux[i][j]=crear_date(json.getJSONObject(i).get(keys.getString(j)));break;
+                    default: aux[i][j]=json.getJSONObject(i).get(keys.getString(j));break;                
+                }
+                System.out.print(aux[i][j]+" ");
+            }                           
+           System.out.println();
+        }
+                                         
+        
+        
+        
+        
+        
+        return aux;
+    }
+    
+    
+    
     
     
     public static void main(String args[]) {
         PruebaJsonNico pru=new PruebaJsonNico();
-        pru.Peticion_nico(0);
+        pru.Peticion_Citas(0);
         
     }
     
