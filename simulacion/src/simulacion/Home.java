d4afef1fed3ef24e34007c037468d19177896d0b
Luis Vivas <luisvivas2504@gmail.com>
2015-12-22 23:49:57 -0430

    Merge origin/master
    Conflicts:
    	src/DB/Bien.java
    	src/inventarioh/MenuBar/Configuracion/AgregarEditar_familia.form
    	src/inventarioh/MenuBar/Configuracion/AgregarEditar_familia.java

--- simulacion/src/simulacion/Home.java
+++ simulacion/src/simulacion/Home.java
@@ -374,10 +374,10 @@ public class Home extends javax.swing.JFrame {
         inventario_ini = Integer.parseInt(inventario_inicial.getText());
         double[] array = Funciones.fread_aleatorios();
         int dia_orden=0;
-        int costo_faltante = 0;
+        double costo_faltante = 0;
         int costoOrden = Integer.parseInt(costo_orden.getText());
-        int costoInventario = 0;
-        int costo_total;
+        double costoInventario = 0;
+        double costo_total = 0;
         int dia=0;
         Clase_retorno clase;
         //int[][] array_clientes;
@@ -386,7 +386,9 @@ public class Home extends javax.swing.JFrame {
         for(int i = 1 ; i <= 15; i++){  
             /* if para ver si ya la orden llego*/
             if(tiempo_espera + dia_orden < i && dia_orden != 0){
-                inventario_ini = inventario_ini + Q;
+                //clase = Funciones.fllegada_pedidos(lista_clientes,Q)
+                //inventario_ini = clase.getQ;
+                //lista_clientes = clase.getList;
                 dia_orden = 0;
             }
             /* aleatorio demanda*/
@@ -399,9 +401,7 @@ public class Home extends javax.swing.JFrame {
             /* si inventario es negativo, es decir, hay faltante */
             if(inventario_fin < 0){
                 faltante = Math.abs(inventario_fin);
-                //costo_faltante = costo_faltante + faltante;
                 lista_clientes.add(new double[] {Funciones.fcompare(aleatorio_demanda,matriz_acum_espera) , faltante});
-                //clase = Funciones.fespera_clientes(lista_clientes);
                 inventario_fin = 0;
                 /* inventario_promedio */
                 inventario_promedio = (inventario_ini + inventario_fin) / 2;
@@ -414,6 +414,10 @@ public class Home extends javax.swing.JFrame {
                     numero_orden++;
                     /* dia en el que se pidio la orden */
                     dia_orden = i;
+                    
+                    //clase = Funciones.fespera_clientes(lista_clientes);
+                    //lista_clientes = clase.getList;
+                    
                     System.out.printf("%d\t|%d\t|%.2f\t|%d\t|%d\t|%d\t|%d\t|%d\t|%.2f\t|%d\t|%.2f\t|%d\t| %n", i,inventario_ini,array[i-1],demanda_diaria,inventario_fin,inventario_promedio,faltante,numero_orden,aleatorio_demanda,tiempo_espera,aleatorio_demanda,Funciones.fcompare(aleatorio_demanda,matriz_acum_espera));
 
                 }else{
@@ -442,10 +446,16 @@ public class Home extends javax.swing.JFrame {
             inventario_ini = inventario_fin;
             dia = i;
         }
-        costoInventario = costoInventario * (Integer.parseInt(costo_inventario.getText()) / 365 );
+        /* Costos */
+        System.out.println("Costos Inventario = " + costoInventario);
+        costoInventario = costoInventario * (Double.parseDouble(costo_inventario.getText()) / 365 );
         costoOrden = dia * costoOrden;
-        
+        costo_faltante = Funciones.fcosto_faltante(Double.parseDouble(costo_nespera.getText()),Double.parseDouble(costo_espera.getText()) );
         costo_total = costoInventario + costoOrden + costo_faltante;
+                
+        System.out.println("Costos Inventario = " + costoInventario);
+        System.out.println("Costos Orden = " + costoOrden);
+        System.out.println("Costos Faltante = " + costo_faltante);
         
         System.out.println("Costo total = " + costo_total);
     }//GEN-LAST:event_startActionPerformed
